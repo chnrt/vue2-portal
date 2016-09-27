08062f4aab7dc918db9f03ba863b402a5a01b021
IMustTo <cxjbinfo@gmail.com>
2016-09-27 22:15:53 +0800

    :pencil: change slider

--- src/components/ImgSlider.vue
+++ src/components/ImgSlider.vue
@@ -1,55 +1,70 @@
-<!-- <template>
-  <div class="abs-content">
-    <div class="slider-content">
-      <div class="slider">
-        <a href="#" class="item"><img src="images/s1.jpg"></a>
-        <a href="#" class="item"><img src="images/s2.jpg"></a>
-      </div>
-    </div>
-  </div>
-</template> -->
 <template>
-  <slider :pages="someList" :sliderinit="sliderinit">
+<slider :pages="someList" :slide="slide">
+
     <!-- slot  -->
-  </slider>
+    <template v-for="(item, index) in someList">
+      <div 
+        class="slider-item" 
+        :style="someList[index].style">
+        <h1>{{item.title}}</h1>
+      </div>
+    </template>
+
+</slider>
 </template>
 
 <script>
 import Slider from './Slider';
 
 export default {
-  name: 'img-slider',
-
   data() {
     return {
-      // 图片列表[arr]
       someList: [{
-        title: 'slide1',
+        title: '1',
+        img: 'testimg-1.png',
+        // slide init
+        origin: 0,
+        current: 0,
         style: {
-          background: '#4bbfc3',
+          'background-image': 'url(dist/testimg-1.png)',
+          'background-size': 'cover',
+          // transform
+          transform: 'translateX(0%)',
         },
       }, {
-        title: 'slide2',
+        title: '2',
+        img: 'testimg-2.png',
+
+        origin: 100,
+        current: 0,
         style: {
-          background: '#4bbfc3',
+          'background-image': 'url(dist/testimg-2.png)',
+          'background-size': 'cover',
+          transform: `translateX(${100}%)`,
         },
       }, {
-        title: 'slide3',
+        title: '3',
+        img: 'testimg-3.png',
+        origin: 200,
+        current: 0,
         style: {
-          background: '#4bbfc3',
+          'background-image': 'url(dist/testimg-3.png)',
+          'background-size': 'cover',
+          transform: `translateX(${200}%)`,
         },
       }],
-
-     // 滑动配置[obj]
-      sliderinit: {
-        currentPage: 0,
-        start: {},
-        end: {},
-        tracking: false,
-        thresholdTime: 500, // 滑动判定距离
-        hresholdDistance: 100, // 滑动判定时间
-        autoplay: 2000, // 自动滚动[ms]
-        loop: true, // 循环滚动
+      slide: {
+        init: {
+          pageNum: 3,
+          currentPage: 1,
+          canPre: false,
+          canNext: true,
+          start: {},
+          end: {},
+          tracking: false,
+          thresholdTime: 500,
+          thresholdDistance: 100,
+        },
       },
     };
   },
