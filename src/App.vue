b48c617042152f231fd6f26bd657ac7d7a79129f
IMustTo <cxjbinfo@gmail.com>
2016-09-27 13:05:39 +0800

    add layout.vue

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,11 @@
     <bg-divider></bg-divider>
 
     <text-box content="常用应用"></text-box>
-    <app-box></app-box>
+    <app-box
+      v-for="(item, index) in appBoxs"
+      :title="item.title">
+    </app-box>
 
-    <bg-divider></bg-divider>
-
-    <div class="col-1of1">
-      <div class="col-content">
-        <div style="height:2.8rem;" class="iframebox border-gray-e3 border-bottom-1 border-top-1">
-          <iframe width="100%" height="100%" src="http://echarts.baidu.com/gallery/editor.html?c=bar-tick-align"></iframe>
-        </div>
-      </div>
-    </div>
-
-    <bg-divider></bg-divider>
-
-    <div class="col-1of2 col-offsetleft-1of2 col-ml-4 col-mr-8">
-      <div class="col-content">
-        <!--.bx-btn-medium .bx-btn-large 中、大 按钮-->
-        <!--.border-round 圆角-->
-        <!--.border-capsule 胶囊形-->
-        <!--.type-2 图标靠左-->
-        <!--.type-3 图标靠右-->
-        <!--.type-4 图标在上文字在下-->
-        <!--.has-icon 有图标-->
-        <div class="bx-btn touch-active has-icon border-capsule bg-blue-ocean font-white">
-          <div class="bx-icon"><i class="fa fa-search"></i></div>
-          <div class="bx-text">按钮</div>
-        </div>
-      </div>
-    </div>
     <bg-divider></bg-divider>
 
     <foot-bar></foot-bar>
@@ -52,6 +28,24 @@
 
   export default {
     name: 'app',
+
+    data() {
+      return {
+        appBoxs: [{
+          title: '成绩发布',
+        }, {
+          title: '教学计划',
+        }, {
+          title: '圈子',
+        }, {
+          title: '通知公告',
+        }, {
+          title: '问卷调查',
+        }, {
+          title: '选课',
+        }],
+      };
+    },
 
     components: {
       ImgSlider,
